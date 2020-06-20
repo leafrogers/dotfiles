0a22ade31853bef25afbfb72ef5de2d9f616607c
Keith Rogers <150157+keirog@users.noreply.github.com>
2020-06-20 15:34:10 +0100

    Add upgrade script
    Run the upgrade script to upgrade:
    - oh-my-zsh
    - brew and brew cask packages
    - macOS apps via mas
    - node versions via fnm
    - vim-plug and plugins via vim

---
+++ .config/upgrade.zsh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+source ~/.zshrc
+
+echo "# Starting upgrades…"
+echo ""
+echo "## OhMyZsh upgrade starting"
+echo "##"
+upgrade_oh_my_zsh
+echo "##"
+echo "## OhMyZsh upgrade done"
+echo ""
+echo ""
+echo "## NodeJS upgrades starting"
+echo "##"
+n install latest-v12.x
+n install latest-v14.x
+echo "##"
+echo "## NodeJS upgrades done"
+echo ""
+echo ""
+echo "## Homebrew upgrades starting"
+echo "##"
+brew update
+brew upgrade
+#brew cask upgrade
+echo "##"
+echo "## Homebrew upgrades done"
+echo ""
+echo ""
+echo "## vim upgrades starting"
+echo "##"
+vim -c 'PlugUpgrade|q|q'
+vim -c 'PlugUpdate|q|q'
+echo "##"
+echo "## vim upgrades done"
+echo ""
+echo ""
+echo "## macOS upgrades starting"
+echo "##"
+mas upgrade
+echo "##"
+echo "## macOS upgrades done"
+echo ""
+echo "# Upgrades done"
